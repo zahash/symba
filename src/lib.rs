0c9ae2ab6f5aa0176328381705883919b47707b4
zahash <zahash.z@gmail.com>
2024-02-13 19:38:41 +0530

    simplify polynomial

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,6 @@
 use std::fmt::Display;
 
 #[derive(Debug, PartialEq, Clone)]
-pub struct Sym(pub String);
-
-#[derive(Debug, PartialEq, Clone)]
-pub struct Deg(pub usize);
-
-#[derive(Debug, PartialEq, Clone)]
 pub struct Poly(pub Vec<PolyTerm>);
 
 #[derive(Debug, PartialEq, Clone)]
@@ -17,8 +11,8 @@ pub struct PolyTerm {
 
 #[derive(Debug, PartialEq, Clone)]
 pub struct PolyVar {
-    pub sym: Sym,
-    pub deg: Deg,
+    pub sym: String,
+    pub deg: usize,
 }
 
 impl Poly {
@@ -27,12 +21,12 @@ impl Poly {
         // add coeffs of like terms
     }
 
-    pub fn substitute(&mut self, sym: &Sym, val: f64) {
+    pub fn substitute(&mut self, sym: &str, val: f64) {
         for term in &mut self.0 {
             for var in &mut term.vars {
-                if &var.sym == sym {
-                    term.coeff *= val.powi(var.deg.0 as i32);
-                    var.deg.0 = 0;
+                if var.sym == sym {
+                    term.coeff *= val.powi(var.deg as i32);
+                    var.deg = 0;
                 }
             }
         }
@@ -71,10 +65,14 @@ impl Display for PolyTerm {
 
 impl Display for PolyVar {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self.deg.0 {
-            1 => write!(f, "{}", self.sym.0),
-            _ => write!(f, "{}{}", self.sym.0, self.deg.0),
+        match self.sym.len() == 1 {
+            true => write!(f, "{}", self.sym)?,
+            false => write!(f, "({})", self.sym)?,
         }
+        if self.deg != 1 {
+            write!(f, "{}", self.deg)?;
+        }
+        Ok(())
     }
 }
 
@@ -88,38 +86,38 @@ mod tests {
             PolyTerm {
                 coeff: 1.,
                 vars: vec![PolyVar {
-                    sym: Sym("x".to_string()),
-                    deg: Deg(2),
+                    sym: "x".to_string(),
+                    deg: 2,
                 }],
             },
             PolyTerm {
                 coeff: -2.,
                 vars: vec![
                     PolyVar {
-                        sym: Sym("x".to_string()),
-                        deg: Deg(3),
+                        sym: "x".to_string(),
+                        deg: 3,
                     },
                     PolyVar {
-                        sym: Sym("y".to_string()),
-                        deg: Deg(1),
+                        sym: "yy".to_string(),
+                        deg: 1,
                     },
                     PolyVar {
-                        sym: Sym("z".to_string()),
-                        deg: Deg(2),
+                        sym: "z".to_string(),
+                        deg: 2,
                     },
                 ],
             },
             PolyTerm {
                 coeff: 10.,
                 vars: vec![PolyVar {
-                    sym: Sym("y".to_string()),
-                    deg: Deg(2),
+                    sym: "y".to_string(),
+                    deg: 2,
                 }],
             },
         ]);
 
         println!("{}", p);
-        p.substitute(&Sym("x".to_string()), 2.);
+        p.substitute("x", 2.);
         println!("{}", p);
     }
 }
